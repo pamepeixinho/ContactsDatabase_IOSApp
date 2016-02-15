402b1f1878992917fae8dba3c3910dfec0571897
Pamela Peixinho <pameiupip@gmail.com>
2016-02-15 10:55:59 -0200

    Initial Commit

---
+++ contactDatabaseApp/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  contactDatabaseApp
+//
+//  Created by Pamela Iupi Peixinho on 2/15/16.
+//  Copyright © 2016 PamelaPeixinho. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ contactDatabaseApp/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  contactDatabaseApp
+//
+//  Created by Pamela Iupi Peixinho on 2/15/16.
+//  Copyright © 2016 PamelaPeixinho. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
